cb8773b0c5442489eb7ebf35866b7e130dfc8538
cako <cakorocamora@gmail.com>
2022-08-17 13:34:35 +0200

    Initial page & about us done

--- gg_frontend/pages/_app.js
+++ gg_frontend/pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import Header from "../components/Header";
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { MoralisProvider } from "react-moralis";
+import Head from "next/head";
+import SocialMedia from "../components/SocialMedia";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider initializeOnMount={false}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Header />
+      <SocialMedia />
+      <Component {...pageProps} />
+      <Footer />
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
